322d20bc8c7d48311372f9fe913b7ee4abdf2db2
isuru <isuro.89@gmail.com>
2022-07-30 15:30:08 +0800

    complete submit application

--- cypress/e2e/onboarding.cy.js
+++ cypress/e2e/onboarding.cy.js
@@ -1,49 +1,59 @@
 /// <reference types="Cypress" />
- import applyHomagePage from '../page/homagedemo/applyHomage'
+import applyHomagePage from '../page/homagedemo/applyHomage'
 
 describe('Get URL', () => {
-  it('Access the homage homepage', () => {
-   cy.visit('https://apply.homage.sg/');
-  })
+    it('Access the homage homepage', () => {
+        cy.visit('https://apply.homage.sg/');
+    })
 
-  describe('Enter user details', () => {
-    it("Submit user details", () => {
-      applyHomagePage.typeFirstname('Michael');
-      applyHomagePage.typeLastname('Jordan');
+    describe('Enter user details', () => {
+        it("Successfully submit user details", () => {
 
-      var chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
-      var randomString = '';
-      for (var i = 0; i < 15; i++) {
-        randomString += chars[Math.floor(Math.random() * chars.length)];
-      }
-      applyHomagePage.typeEmail(randomString + '@homage.com');
-      
-      applyHomagePage.typePhone('82860123');
-      applyHomagePage.typeDob('12/10/1989');
-      applyHomagePage.clickNationality();
-      applyHomagePage.selectNationality();
+            applyHomagePage.clickLocation();
+            applyHomagePage.slelectLocation();
+            //Basic Details
+            applyHomagePage.typeFirstname('Michael');
+            applyHomagePage.typeLastname('Jordan');
 
-      cy.get(':nth-child(6) > .h > .cpf-r20').click()
-      cy.get(':nth-child(7) > .cpf-sec > :nth-child(2)').click()
-      cy.get(':nth-child(9) > :nth-child(1)').click()
+            var chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+            var randomString = '';
+            for (var i = 0; i < 15; i++) {
+                randomString += chars[Math.floor(Math.random() * chars.length)];
+            }
+            applyHomagePage.typeEmail(randomString + '@homage.com');
 
-      cy.get(':nth-child(11) > .cpf-sec > :nth-child(1)').click()
-      cy.get(':nth-child(12) > .cpf-sec > :nth-child(2)').click()
-      cy.get(':nth-child(16) > .cpf-r20').click()
+            applyHomagePage.typePhone('82860123');
+            applyHomagePage.typeDob('12/10/1989');
+            applyHomagePage.clickNationality();
+            applyHomagePage.selectNationality();
+            //Roles
+            applyHomagePage.clickYes();
+            applyHomagePage.clickExperience();
+            applyHomagePage.clickRoles();
+            // Job Opportunities
+            applyHomagePage.clickJobs();
+            // Engagement Types
+            applyHomagePage.clickEngagement();
+            //   Background Check
+            applyHomagePage.clickNo();
+            applyHomagePage.clickHearAbout();
+            applyHomagePage.selectHearAbout();
+            // Declaration
+            applyHomagePage.clickAcknowledgement();
+            applyHomagePage.clickSubmit()
+            //Verify the title after submit
+            applyHomagePage.elements.titleHeader().should('have.text', 'Your application has been submitted!')
 
-      applyHomagePage.clickHearAbout();
-      applyHomagePage.selectHearAbout();
-
-      cy.get('.cpf-check').click()
-
-      //Click submit
-    // applyHomagePage.clickSubmit()
-     //Verify the title after submit
-     applyHomagePage.elements.titleHeader().should('have.text','Your application has been submitted!')
+        })
 
     })
 
-  })
+    describe('Validate required fields', () => {
+        it("Validate email format | Validate phone number format | Validate required fields ", () => {
+
+        })
+
+    })
 
 })
 
--- cypress/page/homagedemo/applyHomage.js
+++ cypress/page/homagedemo/applyHomage.js
@@ -1,55 +1,96 @@
 
-class applyHomePage{
+class applyHomePage {
 
-    elements ={
-        firstnameInput:() =>   cy.get('#firstName'),
-        lastnameInput:() =>   cy.get('#lastName'),
-        emailInput:() =>   cy.get('#email'),
-        phoneInput:() =>   cy.get('#phone'),
-        dobInput:() =>   cy.get('#dob'),
-        nationalityDDL:() =>   cy.get('#nationality'),
-        nationalitySelect:()=> cy.get('[data-value="Singapore Citizen"]'),
-        hearAboutSelect:()=> cy.get('[data-value="YouTube"]'), 
-        hearAboutDDL: ()=> cy.get('#source'),
-        submitBtn:() => cy.get('.cpf-submit'),
-        titleHeader:()=> cy.get('h1'),
-        
+    elements = {
+        locationDDL: () => cy.get('#location'),
+        locationSelect: () => cy.get('.MuiList-root > [tabindex="0"]'), // Need to change this elementID to a stable one.
+        firstnameInput: () => cy.get('#firstName'),
+        lastnameInput: () => cy.get('#lastName'),
+        emailInput: () => cy.get('#email'),
+        phoneInput: () => cy.get('#phone'),
+        dobInput: () => cy.get('#dob'),
+        nationalityDDL: () => cy.get('#nationality'),
+        nationalitySelect: () => cy.get('[data-value="Singapore Citizen"]'),
+        hearAboutSelect: () => cy.get('[data-value="YouTube"]'),
+        hearAboutDDL: () => cy.get('#source'),
+        submitBtn: () => cy.get('.cpf-submit'),
+        titleHeader: () => cy.get('h1'),
+        yesBtn: () => cy.contains('Yes'),
+        noBtn:() => cy.contains('No'),
+        selectExperience: () =>  cy.contains('4 to 6'),
+        selectRoles: () => cy.contains('Therapy Assistant'),
+        selectJobOpportunities: () => cy.contains('Covid Assignments'),
+        selectEngagementTypes: () => cy.contains('Freelance'),
+        checkAcknowledgement: () => cy.get('.cpf-check'),
+
+
     }
 
-typeFirstname(firstname){
-    this.elements.firstnameInput().type(firstname);
-}
-typeLastname(lastname){
-    this.elements.lastnameInput().type(lastname);
-}
-typeEmail(email){
-    this.elements.emailInput().type(email);
-}
-typePhone(phone){
-    this.elements.phoneInput().type(phone);
-}
-typeDob(dob){
-    this.elements.dobInput().type(dob);
-}
-
-clickHearAbout(){
-    this.elements.hearAboutDDL().click();
-}
-selectHearAbout(){
-    this.elements.hearAboutSelect().click();
-}
-selectNationality(){
-    this.elements.nationalitySelect().click();
-}
-clickNationality(){
-    this.elements.nationalityDDL().click();
-}
-clickSubmit(){
-this.elements.submitBtn().click();
-}
 
 
+    clickLocation() {
+        this.elements.locationDDL().click();
+    }
+    slelectLocation() {
+        this.elements.locationSelect().click();
+    }
 
+    typeFirstname(firstname) {
+        this.elements.firstnameInput().type(firstname);
+    }
+    typeLastname(lastname) {
+        this.elements.lastnameInput().type(lastname);
+    }
+    typeEmail(email) {
+        this.elements.emailInput().type(email);
+    }
+    typePhone(phone) {
+        this.elements.phoneInput().type(phone);
+    }
+    typeDob(dob) {
+        this.elements.dobInput().type(dob);
+    }
+
+    clickHearAbout() {
+        this.elements.hearAboutDDL().click();
+    }
+    selectHearAbout() {
+        this.elements.hearAboutSelect().click();
+    }
+
+    selectNationality() {
+        this.elements.nationalitySelect().click();
+    }
+    clickNationality() {
+        this.elements.nationalityDDL().click();
+    }
+
+    clickSubmit() {
+        this.elements.submitBtn().click();
+    }
+    clickYes() {
+        this.elements.yesBtn().click();
+    }
+    clickNo() {
+        this.elements.noBtn().click();
+    }
+
+    clickExperience() {
+        this.elements.selectExperience().click();
+    }
+    clickRoles() {
+        this.elements.selectRoles().click();
+    }
+    clickJobs() {
+        this.elements.selectJobOpportunities().click();
+    }
+    clickEngagement() {
+        this.elements.selectEngagementTypes().click();
+    }
+
+    clickAcknowledgement() {
+        this.elements.checkAcknowledgement().click();
+    }
 }
 
 module.exports = new applyHomePage();
